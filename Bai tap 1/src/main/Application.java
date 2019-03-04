daf0c6b6efeda318552f8ee87d0c7f2f0ecd37e7
Nguyen Luu Nhat <nhatnor123@gmail.com>
2019-03-04 17:08:09 +0700

    zz

--- Bai tap 1/src/main/Application.java
+++ Bai tap 1/src/main/Application.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import handle.HandleInput;
 import input.getURL.URLFromFileConfig;
 
-
 public class Application {
 	public static void main(String[] args) throws IOException {
 		HandleInput inputHandle = new HandleInput();
@@ -18,12 +17,11 @@ public class Application {
 		ArrayList<String> listURL = urlFileConfig.readURL("/home/nhatnor123/Desktop/listURL");
 
 		for (String link : listURL) {
-			
+
 			System.out.println("\n" + link);
 			inputHandle.HanleUrl(new URL(link));
 			System.out.println(LocalDateTime.now());
-			
+
 		}
 	}
 }
-
